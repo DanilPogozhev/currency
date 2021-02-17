f1ac0c36e5a3ee49a6483df9bd9db6298401fd74
Danil Pogozhev <danilpogozhev@gmail.com>
2021-02-17 23:08:43 +0300

    Close #12: Implemented multiplication logic for CHF

--- src/test/java/one/distracting/service/currency/money/MoneyTest.java
+++ src/test/java/one/distracting/service/currency/money/MoneyTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import one.distracting.service.currency.Dollar;
+import one.distracting.service.currency.Franc;
 
 public class MoneyTest {
 
@@ -13,6 +14,13 @@ public class MoneyTest {
         Dollar five = new Dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 
 }
