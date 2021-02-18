34bcc0aa07a127c80737b66a9a0a2b7a1cf92df8
Danil Pogozhev <danilpogozhev@gmail.com>
2021-02-18 17:54:41 +0300

    Close #17: Moved constructor methods to super-class.

--- src/main/java/one/distracting/service/currency/model/Money.java
+++ src/main/java/one/distracting/service/currency/model/Money.java
@@ -3,21 +3,31 @@ package one.distracting.service.currency.model;
 public abstract class Money {
 
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    static Money dollar(int amount) {
+        return new Dollar(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
+    abstract Money times(int multiplier);
+
+    public String getCurrency() {
+        return currency;
+    }
 
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
-
-    static Money dollar(int amount) {
-        return new Dollar(amount);
-    }
-
-    static Money franc(int amount) {
-        return new Franc(amount);
-    }
-
-    abstract Money times(int multiplier);
 
 }
